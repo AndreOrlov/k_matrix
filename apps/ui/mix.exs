62c67ea8ba4de147a3e7f92db849532149b1d1c6
Andre Orlov <vkstan@mail.ru>
2019-06-04 23:04:34 +0300

    Add CSV deps

--- apps/ui/mix.exs
+++ apps/ui/mix.exs
@@ -43,7 +43,8 @@ defmodule Ui.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ring_logger, "~> 0.6"}
+      {:ring_logger, "~> 0.6"},
+      {:csv, "~> 2.3"}
     ]
   end
 end

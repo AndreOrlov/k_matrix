139474f8395b33842fc26f3de4b8540da317f3bd
Andre Orlov <vkstan@mail.ru>
2019-06-11 09:46:02 +0300

    Fix warning unused params

--- apps/ui/lib/ui_web/controllers/matrix_controller.ex
+++ apps/ui/lib/ui_web/controllers/matrix_controller.ex
@@ -1,7 +1,7 @@
 defmodule UiWeb.MatrixController do
   use UiWeb, :controller
 
-  def upload_file(conn, params) do
+  def upload_file(conn, _params) do
     # TODO: rad
     # Task.Supervisor.async_nolink(Ui.TaskSupervisor, fn -> IO.puts("TEST Task") end)
 

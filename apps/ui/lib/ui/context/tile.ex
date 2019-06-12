f84eaef83bdcc7d07e7e8e8c5094794a0a2d9820
Andre Orlov <vkstan@mail.ru>
2019-06-13 00:59:05 +0300

    Add formatter coords to binaries format

--- apps/ui/lib/ui/context/tile.ex
+++ apps/ui/lib/ui/context/tile.ex
@@ -1,4 +1,52 @@
 defmodule Context.Tile do
+  # dimensions tile
   @cols 8
   @rows 8
+
+  # matrix in tiles
+  @matrix_weight 2
+  @matrix_height 2
+
+  @x [
+    0b10000000,
+    0b01000000,
+    0b00100000,
+    0b00010000,
+    0b00001000,
+    0b00000100,
+    0b00000010,
+    0b00000001
+  ]
+  @x_default 0
+
+  @y [
+    0b00000001,
+    0b00000010,
+    0b00000011,
+    0b00000100,
+    0b00000101,
+    0b00000110,
+    0b00000111,
+    0b00001000
+  ]
+  @y_default 0
+
+  # TODO: def init function
+
+  def coord(x, y) when is_integer(x) and is_integer(y) do
+    IO.inspect({:div, x_div, :rem, x_rem} = div_rem(x, @cols), label: "X")
+    IO.inspect({:div, y_div, :rem, y_rem} = div_rem(y, @rows), label: "Y")
+
+    for y_coord <- 0..(@matrix_height - 1), x_coord <- 0..(@matrix_weight - 1) do
+      case {y_coord, x_coord} do
+        {^y_div, ^x_div} -> [Enum.at(@y, y_rem), Enum.at(@x, x_rem)]
+        _ -> [@y_default, @x_default]
+      end
+    end
+    |> List.flatten()
+    |> Enum.into(<<>>, &<<&1>>)
+  end
+
+  defp div_rem(dividend, divisor),
+    do: {:div, div(dividend, divisor), :rem, rem(dividend, divisor)}
 end

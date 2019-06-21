271cb9e7eabf8069f9b3d2f731b0bbd157963302
Andre Orlov <vkstan@mail.ru>
2019-06-22 00:09:57 +0300

    Refactor test Context.Tile

--- apps/ui/lib/ui/context/tile.ex
+++ apps/ui/lib/ui/context/tile.ex
@@ -6,7 +6,7 @@ defmodule Context.Tile do
   @rows Application.get_env(:matrix, :dimensions)[:tile_rows]
 
   # matrix in tiles
-  @matrix_weight Application.get_env(:matrix, :dimensions)[:weight]
+  @matrix_width Application.get_env(:matrix, :dimensions)[:weight]
   @matrix_height Application.get_env(:matrix, :dimensions)[:height]
 
   # TODO: DYI
@@ -52,31 +52,46 @@ defmodule Context.Tile do
     :ok = Driver.disable_code(ref)
     :ok = Driver.resume(ref)
 
-    :ok = Driver.lights_on_by_coords(ref, coord_tiles(coords))
+    :ok = Driver.lights_on_by_coords(ref, coord(coords))
 
     :ok = Driver.close(ref)
   end
 
-  # TODO: перевести сначала в обычные координаты. В коорд. макс7219 переведет Context.Tile.Max7219
-  # coord x, y - координаты во всей матрице tiles
-  def coord([x | [y | _]] = point_coord) when length(point_coord) == 2, do: coord(x, y)
+  defmacro is_valid_coord(x, y) do
+    quote do
+      is_integer(unquote(x)) and
+        is_integer(unquote(y)) and
+        unquote(x) >= 0 and
+        unquote(y) >= 0 and
+        unquote(x) < @cols * @matrix_width and
+        unquote(y) < @rows * @matrix_height
+    end
+  end
 
-  def coord(x, y) when is_integer(x) and is_integer(y) do
+  # Перевести сначала в обычные координаты и поменять местами x, y.
+  # В коорд. макс7219 переведет Context.Tile.Max7219
+  # coord x, y - координаты во всей матрицеб разбитые по tiles
+  def coord([x, y] = point_coord) when length(point_coord) == 2, do: coord(x, y)
+
+  def coord(x, y) when is_valid_coord(x, y) do
     {:div, x_div, :rem, x_rem} = div_rem(x, @cols)
     {:div, y_div, :rem, y_rem} = div_rem(y, @rows)
 
-    for y_coord <- 0..(@matrix_height - 1), x_coord <- 0..(@matrix_weight - 1) do
+    for y_coord <- 0..(@matrix_height - 1), x_coord <- 0..(@matrix_width - 1) do
       case {y_coord, x_coord} do
-        {^y_div, ^x_div} -> [Enum.at(@y, y_rem), Enum.at(@x, x_rem)]
-        _ -> [@y_default, @x_default]
+        {^y_div, ^x_div} ->
+          [y_rem, x_rem]
+
+        _ ->
+          []
       end
     end
   end
 
-  # coords [[x1, y1], ...,[xn, yn]], координаты в каждом tile (8 х 8). length(coords) == кол-во tiles (8 x 8) в матрице
-  def coord_tiles(coords) do
+  # coords [[x1, y1], ...,[xn, yn]], координаты в каждом tile.
+  def coord(coords) do
     coords
-    |> Enum.map(fn [x | [y | _]] -> coord(x, y) end)
+    |> Enum.map(fn [x, y] -> coord(x, y) end)
   end
 
   defp div_rem(dividend, divisor),

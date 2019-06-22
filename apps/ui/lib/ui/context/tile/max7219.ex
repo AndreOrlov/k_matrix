373858582885fb0cc60ddc06a7593a97d5d76527
Andre Orlov <vkstan@mail.ru>
2019-06-22 23:28:51 +0300

    Revert "Fix gruop_by_row and test passed"
    This reverts commit 7006e7c2857ca357c2fc44e15c8db699cfd22ee3.

--- apps/ui/lib/ui/context/tile/max7219.ex
+++ apps/ui/lib/ui/context/tile/max7219.ex
@@ -194,17 +194,8 @@ defmodule Context.Tile.Max7219 do
   defp group_x_by_y(coord, []), do: [coord]
 
   defp group_x_by_y(coord, acc) do
-    [y_coord, x_coord] =
-      case coord do
-        [_, _] -> coord
-        _ -> [0, 0]
-      end
-
-    [y_acc, x_acc] =
-      case res = Enum.at(acc, -1) do
-        [y, x] -> [y, x]
-        [] -> [0, 0]
-      end
+    [y_coord, x_coord] = coord
+    [y_acc, x_acc] = Enum.at(acc, -1)
 
     cond do
       y_coord == y_acc ->

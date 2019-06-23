dc7683548df33311ab2d55f33be3d3d7ba9d2af6
Andre Orlov <vkstan@mail.ru>
2019-06-23 16:03:07 +0300

    Refactor a bit

--- apps/ui/lib/ui/context/tile.ex
+++ apps/ui/lib/ui/context/tile.ex
@@ -9,8 +9,26 @@ defmodule Context.Tile do
   @matrix_width Application.get_env(:matrix, :dimensions)[:weight]
   @matrix_height Application.get_env(:matrix, :dimensions)[:height]
 
-  # Инициализирует матрицу диодов, формирование SPI команд
   def init() do
+    __sender__(&__test_leds__/1)
+  end
+
+  # coords [[x1, y1], ... ,[xn, yn]]
+  def send_coords(coords) do
+    __sender__(fn ref ->
+      :ok = Driver.lights_on_by_coords(ref, __coord_by_tiles__(coords))
+    end)
+  end
+
+  def __test_leds__(ref) do
+    # Проверка исправности диодов
+    :ok = Driver.test_on(ref)
+    Process.sleep(3000)
+    :ok = Driver.test_off(ref)
+  end
+
+  # Инициализирует матрицу диодов, формирование SPI команд
+  def __sender__(callback) when is_function(callback) do
     {:ok, ref} = Driver.open()
 
     # Не горят диоды, но команды можно отправлять при ткаом режиме
@@ -29,28 +47,7 @@ defmodule Context.Tile do
     # Погасить все, ранее включенные диоды
     :ok = Driver.lights_off(ref)
 
-    # Проверка исправности диодов
-    :ok = Driver.test_on(ref)
-    Process.sleep(3000)
-    :ok = Driver.test_off(ref)
-
-    # Выйти из режима shutdown
-    :ok = Driver.resume(ref)
-
-    :ok = Driver.close(ref)
-  end
-
-  # coords [[x1, y1], ... ,[xn, yn]]
-  def send_coords(coords) do
-    {:ok, ref} = Driver.open()
-
-    # Не горят диоды, но команды можно отправлять при ткаом режиме
-    :ok = Driver.shutdown(ref)
-
-    # Погасить все, ранее включенные диоды
-    :ok = Driver.lights_off(ref)
-
-    :ok = Driver.lights_on_by_coords(ref, __coord_by_tiles__(coords))
+    callback.(ref)
 
     # Выйти из режима shutdown
     :ok = Driver.resume(ref)

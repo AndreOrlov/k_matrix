88c76798f54aa5a93608653d18345fee5c0d71dc
Andre Orlov <vkstan@mail.ru>
2019-06-24 19:59:01 +0300

    Render image in template

--- apps/ui/lib/ui_web/views/matrix_view.ex
+++ apps/ui/lib/ui_web/views/matrix_view.ex
@@ -4,4 +4,9 @@ defmodule UiWeb.MatrixView do
   def to_json(data) do
     Jason.encode!(data)
   end
+
+  def render("image.html", opts) do
+    opts
+    render_template("image.html", opts)
+  end
 end
